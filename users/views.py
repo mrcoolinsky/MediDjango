218bb6f52851c51efcecade0928885026a59e96a
WiktorKulinski <77587834+WiktorKulinski@users.noreply.github.com>
2023-11-07 11:06:37 +0100

    Added login form

--- users/views.py
+++ users/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render, redirect
-from .forms import CreateUserForm
+from .forms import CreateUserForm, LoginForm
 
-
-# Create your views here.
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate, login, logout
 
 
 def login(request):
-    return render(request, 'users/login.html')
+    form = LoginForm()
+
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+                return redirect("register")
+    context = {'loginform': form}
+    return render(request, 'users/login.html', context=context)
 
 
 def register(request):
