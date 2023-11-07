5de9b99295174126241e156f92f906e4192fa5d4
WiktorKulinski <77587834+WiktorKulinski@users.noreply.github.com>
2023-11-07 11:26:07 +0100

    fix logout url

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
 
     path('register', views.register, name="register"),
     path('login', views.login, name="login"),
-    path('user_logout', views.user_logout(), name="user_logout"),
+    path('user_logout', views.user_logout, name="user_logout"),
 ]
