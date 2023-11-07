19797e6af882c83b93bc567c5ac69ad5f9afdc4d
WiktorKulinski <77587834+WiktorKulinski@users.noreply.github.com>
2023-11-07 10:43:07 +0100

    Added registration form

--- users/views.py
+++ users/views.py
@@ -1,14 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm
 
 
 # Create your views here.
 
 
 def login(request):
-
     return render(request, 'users/login.html')
 
-def register(request):
 
-    return render(request, 'users/register.html')
+def register(request):
+    form = CreateUserForm()
+
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+        return redirect("login")
+
+    context = {'reformist': form}
+    return render(request, 'users/register.html', context=context)
