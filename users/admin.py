6c42312cb816d602d23f94d4fb9d54507e124ce6
WiktorKulinski <77587834+WiktorKulinski@users.noreply.github.com>
2023-11-09 23:41:04 +0100

    added two models into database - Address, Doctors

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Doctor, Address
 # Register your models here.
+
+admin.site.register(Doctor)
+admin.site.register(Address)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Address(models.Model):
+    Street = models.TextField()
+    Number = models.TextField()
+    Zip_code = models.TextField()
+    City = models.TextField()
+
+
+class Doctor(models.Model):
+    user = models.OneToOneField(User, null=False, on_delete=models.CASCADE)
+    address = models.OneToOneField(Address, null=False, on_delete=models.CASCADE, default="")
+    Name = models.TextField(max_length=20, null=False, default="")
+    Surname = models.TextField(max_length=20, null=False, default="")
+    Specialization = models.TextField(max_length=50, null=False, default="")
+
+    def __str__(self):
+        return str(self.user)
