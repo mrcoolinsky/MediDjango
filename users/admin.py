ac5e5d8d41a4511316135ee03fc7dda3a9bef166
WiktorKulinski <77587834+WiktorKulinski@users.noreply.github.com>
2023-11-15 20:23:45 +0100

    database optimalization

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Doctor, Address
+from .models import Doctor, Patient, Visit, Documentation, Medicine, Disease
 # Register your models here.
 
 admin.site.register(Doctor)
-admin.site.register(Address)
+admin.site.register(Patient)
+admin.site.register(Visit)
+admin.site.register(Documentation)
+admin.site.register(Medicine)
+admin.site.register(Disease)
--- users/models.py
+++ users/models.py
@@ -12,9 +12,51 @@ class Address(models.Model):
 class Doctor(models.Model):
     user = models.OneToOneField(User, null=False, on_delete=models.CASCADE)
     address = models.OneToOneField(Address, null=False, on_delete=models.CASCADE, default="")
-    Name = models.TextField(max_length=20, null=False, default="")
-    Surname = models.TextField(max_length=20, null=False, default="")
-    Specialization = models.TextField(max_length=50, null=False, default="")
+    Name = models.CharField(max_length=20, null=False, default="")
+    Surname = models.CharField(max_length=20, null=False, default="")
+    Specialization = models.CharField(max_length=50, null=False, default="")
 
     def __str__(self):
         return str(self.user)
+
+
+class Medicine(models.Model):
+    title = models.CharField(null=False, default="", max_length=20)
+    property = models.CharField(null=True, default="", max_length=50)
+    availability = models.BooleanField(default=False)
+
+
+class Disease(models.Model):
+    title = models.CharField(null=False, default="", max_length=20)
+    property = models.CharField(null=True, default="", max_length=50)
+
+class Documentation(models.Model):
+    medicines = models.ManyToManyField(Medicine)
+    diseases = models.ManyToManyField(Disease)
+    visits = models.ManyToManyField(Visit)
+
+class Patient(models.Model):
+    user = models.OneToOneField(User, null=False, on_delete=models.CASCADE)
+    address = models.OneToOneField(Address, null=False, on_delete=models.CASCADE, default="")
+    Name = models.CharField(max_length=20, null=False, default="")
+    Surname = models.CharField(max_length=20, null=False, default="")
+    DateOfBirth = models.DateField(max_length=10, null=False, default="")
+    Documentation = models.OneToOneField(Documentation, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.user)
+
+class Visit(models.Model):
+    title = models.CharField(null=False, default="", max_length=20)
+    patient = models.OneToOneField(Patient, null=False, on_delete=models.CASCADE)
+    doctor = models.OneToOneField(Doctor, null=False, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return str(self.title)
+
+
+
+
+
+
