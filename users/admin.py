e95dc8c7bfb06f90f421bd3c059a1122b461cb2b
WiktorKulinski <77587834+WiktorKulinski@users.noreply.github.com>
2023-11-16 00:24:29 +0100

    database optimalization, added models

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Doctor, Patient, Visit, Documentation, Medicine, Disease
+from .models import Doctor, Patient, Visit, Documentation, Medicine, Disease, Address
 # Register your models here.
 
 admin.site.register(Doctor)
@@ -8,3 +8,4 @@ admin.site.register(Visit)
 admin.site.register(Documentation)
 admin.site.register(Medicine)
 admin.site.register(Disease)
+admin.site.register(Address)
