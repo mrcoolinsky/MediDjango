d84d287312aff1979cab8a40cd92756ff85959cf
konzii <konradjdr@outlook.com>
2023-12-14 13:46:29 +0100

    initial commit

---
+++ main/templatetags/have_group.py
@@ -0,0 +1,18 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='is_receptionist')
+def is_receptionist(user):
+    return user.groups.filter(name='Recepcjonista').exists()
+
+
+@register.filter(name='is_doctor')
+def is_doctor(user):
+    return user.groups.filter(name='Lekarz').exists()
+
+
+@register.filter(name='is_patient')
+def is_doctor(user):
+    return user.groups.filter(name='Pacjent').exists()
