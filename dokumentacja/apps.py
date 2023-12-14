dd03cae5302baf04c43e8601b608a029b0317cab
konzii <konradjdr@outlook.com>
2023-12-14 13:40:57 +0100

    initial commit

---
+++ dokumentacja/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DokumentacjaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dokumentacja'
--- dokumentacja/urls.py
+++ dokumentacja/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
     path('', include('main.urls')),
-    path('', include('dokumentacja.urls'))
+    path('', views.index, name='index')
 ]
